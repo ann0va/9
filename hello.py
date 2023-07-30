718cf2bdce604eecaf890d8a274f25f7e7aedae5
ann0va <anna.zhosan7@gmail.com>
2023-07-30 15:04:46 +0200

    adding files

---
+++ hello.py
@@ -0,0 +1,4 @@
+def hello() -> str:
+    name = input("Enter your name:")
+    greeting = f'Hello{name}'
+    return greeting
---
+++ main.py
@@ -0,0 +1,8 @@
+from hello import hello 
+
+def main():
+    greeting_message = hello()
+    print(greeting_message)
+
+if __name__ == "__main__":
+    main()
